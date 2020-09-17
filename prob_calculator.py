d8684ddb8d0c24aea8c81c2aac97ea73b1e922eb
Debojeet Das <debojeetofficial@gmail.com>
2020-09-17 15:25:19 +0000

    added hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -4,5 +4,24 @@ import random
 
 class Hat:
 
+  #Instansiation
+  def __init__(self, **balls):
+    self.contents = list()
+    for (k, v) in balls.items:
+      for x in v:
+        self.contents.append(k)
+
+  #Defining draw method
+  def draw(self, num):
+    tmp = copy.copy(self.contents)
+    if num >= len(tmp):
+      return tmp
+    else:
+      out = list()
+      for x in num:
+        out.append(tmp.pop(random.randrange(len(tmp))))
+      return out
+
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
